f557047732cacd4407ef12be4abc446a65446b82
boris-p <3728381+boris-p@users.noreply.github.com>
2026-01-25 23:31:34 -0700

    github pages base route

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/startup-options-calculator">
       <App />
     </BrowserRouter>
   </StrictMode>,
